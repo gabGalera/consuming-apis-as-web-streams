e192b7ea12f758a078697e8d4700c6d61e38462a
Gabriel Galera <gabgalera@hotmail.com>
2023-10-30 13:56:48 -0300

    reading data, transforming it in json and sending it

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 import { createServer } from "node:http";
 import { createReadStream } from "node:fs";
 import { Readable, Transform } from "node:stream";
-import { WritableStream } from "node:stream/web";
+import { WritableStream, TransformStream } from "node:stream/web";
+import { setTimeout } from "node:timers/promises";
 import csvtojson from "csvtojson";
 
 const PORT = 3000;
@@ -16,19 +17,51 @@ createServer(async (request, response) => {
     response.end();
     return;
   }
+  let items = 0;
+  const abortController = new AbortController();
+  request.once("close", () => {
+    console.log(`Connection was closed! ${items} processed...`);
+    abortController.abort();
+  });
 
-  Readable.toWeb(createReadStream("./animeflv.csv"))
-    .pipeThrough(Transform.toWeb(csvtojson()))
-    .pipeTo(
-      new WritableStream({
-        write(chunk) {
-          response.write(chunk);
-        },
-        close() {
-          response.end();
-        },
-      })
-    );
+  try {
+    await Readable.toWeb(createReadStream("./animeflv.csv"))
+      .pipeThrough(Transform.toWeb(csvtojson()))
+      .pipeThrough(
+        new TransformStream({
+          transform(chunk, controller) {
+            const data = JSON.parse(Buffer.from(chunk));
+            const mappedData = JSON.stringify({
+              title: data.title,
+              description: data.description,
+              url: data.url_anime,
+            });
+            console.log({ mappedData });
+            controller.enqueue(mappedData.concat("\n"));
+          },
+        })
+      )
+      .pipeTo(
+        new WritableStream({
+          async write(chunk) {
+            await setTimeout(200);
+            items++;
+            response.write(chunk);
+          },
+          //   abort(reason) {
+          //     console.log("aborted***", reason);
+          //   },
+          close() {
+            response.end();
+          },
+        }),
+        {
+          signal: abortController.signal,
+        }
+      );
+  } catch (error) {
+    if (!error.message.includes("abort")) throw error;
+  }
 })
   .listen(3000)
   .on("listening", (_) => console.log("Application is running at", PORT));
